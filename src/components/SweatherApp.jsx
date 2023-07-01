ded90993ca5b75cdbb3b337952ed49045ffcb28c
RonaldoWongs <deigamerwong@gmail.com>
2023-07-01 01:31:02 -0600

    SECOND COMMIT

--- src/components/SweatherApp.jsx
+++ src/components/SweatherApp.jsx
@@ -6,46 +6,71 @@ import temperatura from '../../img/temperatura.png';
 import humedad from '../../img/humedad.png';
 import viento from '../../img/viento.png';
 
-
 const SweatherApp = () => {
   const [climate, setClimate] = useState({});
   const [transform, setTransform] = useState(true);
+  const [city, setCity] = useState('');
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(success);
+    const fetchCurrentCity = async () => {
+      try {
+        const response = await axios.get('https://ipapi.co/json');
+        const currentCity = response.data.city;
+        setCity(currentCity);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-    function success(pos) {
-      const crd = pos.coords;
-
-      axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=3cbb5705eaf1be0736fcd4103d0590e5`
-        )
-        .then((res) => {
-          setClimate(res.data);
-        });
-    }
+    fetchCurrentCity();
   }, []);
 
-  const getBackgroundImage = (weatherCode) => {
-    if (weatherCode >= 200 && weatherCode < 300) {
-      return 'https://fondosmil.com/fondo/57234.jpg'; 
-    } else if (weatherCode >= 300 && weatherCode < 600) {
-      return 'https://images8.alphacoders.com/429/429517.jpg'; 
-    } else if (weatherCode >= 600 && weatherCode < 700) {
-      return 'https://www.todofondos.net/wp-content/uploads/3840x2160-Invierno-Sunset-con-nieve-4K-Ultra-HD-Wallpaper-scaled.jpg'; 
-    } else if (weatherCode >= 700 && weatherCode < 800) {
-      return 'https://images5.alphacoders.com/103/1035809.jpg'; 
-    } else if (weatherCode === 800) {
-      return 'https://www.wallpaperflare.com/static/1005/114/442/fife-sunset-photography-green-wallpaper.jpg'; 
-    } else if (weatherCode > 800 && weatherCode < 900) {
-      return 'https://esferacomunicacional.ar/wp-content/uploads/2021/03/cumulus_tipos-de-nubes.jpg'; 
-    } else {
-      return ''; 
+  useEffect(() => {
+    fetchWeatherData(city);
+  }, [city]);
+
+  const fetchWeatherData = (city) => {
+    if (city.trim() === '') {
+      return;
     }
+    setLoading(true);
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3cbb5705eaf1be0736fcd4103d0590e5`
+      )
+      .then((res) => {
+        setClimate(res.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
   };
 
-  const backgroundImage = getBackgroundImage(climate.weather?.[0].id);
+  const getBackgroundImage = (weatherCode) => {
+    if (typeof weatherCode === 'number') {
+      if (weatherCode >= 200 && weatherCode < 300) {
+        return 'https://fondosmil.com/fondo/57234.jpg';
+      } else if (weatherCode >= 300 && weatherCode < 600) {
+        return 'https://i.gifer.com/GYlk.gif';
+      } else if (weatherCode >= 600 && weatherCode < 700) {
+        return 'https://www.todofondos.net/wp-content/uploads/3840x2160-Invierno-Sunset-con-nieve-4K-Ultra-HD-Wallpaper-scaled.jpg';
+      } else if (weatherCode >= 700 && weatherCode < 800) {
+        return 'https://images5.alphacoders.com/103/1035809.jpg';
+      } else if (weatherCode === 800) {
+        return 'https://www.wallpaperflare.com/static/1005/114/442/fife-sunset-photography-green-wallpaper.jpg';
+      } else if (weatherCode > 800 && weatherCode < 900) {
+        return 'https://z101digital.com/wp-content/uploads/2019/03/sol-cielo-nubes-17032019-onamet.gif';
+      } else {
+        return '';
+      }
+    }
+    return '';
+  };
+
+  const backgroundImage = getBackgroundImage(climate.weather?.[0]?.id);
   const bodyStyle = {
     backgroundImage: `url(${backgroundImage})`,
     backgroundSize: 'cover',
@@ -57,39 +82,56 @@ const SweatherApp = () => {
   };
 
   return (
-    <body style={bodyStyle}>
-
+    <div style={bodyStyle}>
       <div className='climate-container'>
         <h1>Weather App</h1>
-        <h2>
-          <img src={mundo}  alt="" /> {climate.sys?.country}
-        </h2>
-        <h2>
-          <img src={mapa} alt="" />{climate.name}
-        </h2>
-        <img
-          className="imgContainer"
-          src={`http://openweathermap.org/img/wn/${climate.weather?.[0].icon}.png`}
-          alt=""
-        />
-        <h2>{climate.weather?.[0].description}</h2>
-        <h2>
-          <img src={temperatura} alt="" />{' '}
-          {transform
-            ? (climate.main?.temp - 273).toFixed(2)
-            : ((climate.main?.temp - 273).toFixed(2) * 9) / 5 + 32}
-          {transform ? ' °C' : ' °F'}
-        </h2>
-        <h2>
-          <img src={humedad} alt="" /> {climate.main?.humidity}
-        </h2>
-        <h2>Pressure: {climate.main?.pressure}</h2>
-        <h2>
-         <img src={viento} alt="" /> {climate.wind?.speed}
-        </h2>
-        <button className='btn-5' onClick={() => setTransform(!transform)}>°C & °F / Units</button>
+        <div className='search-container'>
+          <input
+            type='text'
+            placeholder='Enter city'
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <button onClick={() => fetchWeatherData(city)}>Search</button>
+        </div>
+        {loading ? (
+          <h2>Loading...</h2>
+        ) : (
+          <>
+            <h2>
+              <img src={mundo} alt='' /> {climate.sys?.country}
+            </h2>
+            <h2>
+              <img src={mapa} alt='' />
+              {climate.name}
+            </h2>
+            <img
+              className='imgContainer'
+              src={`http://openweathermap.org/img/wn/${climate.weather?.[0]?.icon}.png`}
+              alt=''
+            />
+            <h2>{climate.weather?.[0]?.description}</h2>
+            <h2>
+              <img src={temperatura} alt='' />{' '}
+              {transform
+                ? (climate.main?.temp - 273).toFixed(2)
+                : ((climate.main?.temp - 273).toFixed(2) * 9) / 5 + 32}
+              {transform ? ' °C' : ' °F'}
+            </h2>
+            <h2>
+              <img src={humedad} alt='' /> {climate.main?.humidity}
+            </h2>
+            <h2>Pressure: {climate.main?.pressure}</h2>
+            <h2>
+              <img src={viento} alt='' /> {climate.wind?.speed}
+            </h2>
+            <button className='btn-5' onClick={() => setTransform(!transform)}>
+              °C & °F / Units
+            </button>
+          </>
+        )}
       </div>
-    </body>
+    </div>
   );
 };
 
